572f6ff7904b383b3820d167385fc282c9900349
fantasy <442422060@qq.com>
2020-11-09 10:35:56 +0800

    first commit

---
+++ applets-swiftls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  applets-swiftls
+//
+//  Created by Mars Scala on 2020/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
